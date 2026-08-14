6d0c00a4c2ac075733b60445fdbc55fff7c9b1a8
Ghuilherme29 <guilherme.silva15@ba.estudante.senai.br>
2026-08-14 13:35:21 -0300

    Update Atv4.java

--- Atv4.java
+++ Atv4.java
@@ -1,3 +1,9 @@
 public class Atv4 {
-    
+    public static void main(String[] args) {
+        
+        int Forca = 5;
+        int Distancia = 10;
+        int Trabalho = Forca * Distancia;
+        System.out.println("O trabalho realizado é: " + Trabalho);
+    }
 }
